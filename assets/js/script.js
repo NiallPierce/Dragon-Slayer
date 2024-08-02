db36ccb1e2fa50b58b0aa316eb82cd82fef7d2f9
Niall Pierce <niallpierce92@hotmail.com>
2024-08-02 15:44:39 +0000

    Function for Audio to play when play button is clicked and hides play button once clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
-//Audio file
 let backgroundMusic = new Audio('assets/audio/background-music.mp3');
 backgroundMusic.loop = true;
 let isMuted = false;
 let hasPlayedOnce = false;
 
-      // Play music on button click
-      playButton.addEventListener('click', function playOnUserInteraction() {
-        if (!hasPlayedOnce) {
-            backgroundMusic.play().then(() => {
-                hasPlayedOnce = true;
-                console.log('Music started playing');
-                playButton.style.display = 'none'; // Hides the button after successful play
-                gameContainer.style.display = 'block'; // Shows the game container
-
-                setTimeout(() => gameContainer.classList.add('show'), 10);
-
-            }).catch(error => {
-                console.log('Play failed:', error);
-            });
-        }
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    // Hides the game container and show the play button
+      const gameContainer = document.querySelector('.game-container');
+      gameContainer.style.display = 'none';
+  
+  
+      const playButton = document.createElement('button');
+      playButton.id = 'playButton';
+      playButton.innerHTML = 'PLAY GAME'; // Play Game symbol 
+      document.body.prepend(playButton);
+});
