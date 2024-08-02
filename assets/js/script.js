5ebc118ee1e6fd9966b9b61e8a4979cf768f1b9c
Niall Pierce <niallpierce92@hotmail.com>
2024-08-02 15:35:40 +0000

    Add fuctions to for play button to open main game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
+//Audio file
 let backgroundMusic = new Audio('assets/audio/background-music.mp3');
 backgroundMusic.loop = true;
 let isMuted = false;
 let hasPlayedOnce = false;
+
+      // Play music on button click
+      playButton.addEventListener('click', function playOnUserInteraction() {
+        if (!hasPlayedOnce) {
+            backgroundMusic.play().then(() => {
+                hasPlayedOnce = true;
+                console.log('Music started playing');
+                playButton.style.display = 'none'; // Hides the button after successful play
+                gameContainer.style.display = 'block'; // Shows the game container
+
+                setTimeout(() => gameContainer.classList.add('show'), 10);
+
+            }).catch(error => {
+                console.log('Play failed:', error);
+            });
+        }
+    });
